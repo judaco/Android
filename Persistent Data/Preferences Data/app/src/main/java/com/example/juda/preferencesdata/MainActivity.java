63cd872328c65ebfea634ee091c9f0e72d02678f
Judaco <judacossa@gmail.com>
2017-01-28 14:58:36 +0200

    Shared Preferences
    Shared Preferences - save a message

--- Persistent Data/Preferences Data/app/src/main/java/com/example/juda/preferencesdata/MainActivity.java
+++ Persistent Data/Preferences Data/app/src/main/java/com/example/juda/preferencesdata/MainActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     public static final String MY_PREF = "MyPref";
+    public static final String MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +19,13 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         SharedPreferences sharedPreferences = getSharedPreferences(MY_PREF, MODE_PRIVATE);
+
+        String message = sharedPreferences.getString(MESSAGE, null);
+        if (message != null){
+            TextView lblMessage = (TextView)findViewById(R.id.lblMessage);
+        }
         //how to write to shared preferences
-        SharedPreferences.Editor editor = sharedPreferences.edit();
+       /* SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("username", "juda");
         editor.putInt("age", 27);
         editor.putString("nickname", "jude");
@@ -26,6 +35,17 @@ public class MainActivity extends Activity {
 
         String userName = sharedPreferences.getString("username", null);
         int age = sharedPreferences.getInt("age", -1);
-        String nickName = sharedPreferences.getString("nickname", null);
+        String nickName = sharedPreferences.getString("nickname", null);*/
+    }
+
+    public void btnSave(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences(MY_PREF, MODE_PRIVATE);
+        EditText txtMessage = (EditText)findViewById(R.id.txtMessage);
+        String message = txtMessage.getText().toString();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(MESSAGE, message);
+        editor.commit();
+        TextView lblMessage = (TextView)findViewById(R.id.lblMessage);
+        lblMessage.setText(message);
     }
 }
