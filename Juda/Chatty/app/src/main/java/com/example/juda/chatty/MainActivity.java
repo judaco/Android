b47b942eb9dbfdbd580f13b7b4aeb72785274b91
Judaco <judacossa@gmail.com>
2017-06-03 20:49:22 +0300

    chatty

---
+++ Juda/Chatty/app/src/main/java/com/example/juda/chatty/MainActivity.java
@@ -0,0 +1,248 @@
+package com.example.juda.chatty;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    public static final String ANONYMOUS = "anonymous";
+    public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+    public static final int RC_SIGN_IN = 123;//RC = request code
+
+    private ListView messageListView;
+    private MessageAdapter messageAdapter;
+    private ProgressBar progressBar;
+    private ImageButton photoPickerButton;
+    private EditText messageET;
+    private Button sendButton;
+
+    private String userName;
+
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private ChildEventListener childEventListener;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userName = ANONYMOUS;
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        databaseReference = firebaseDatabase.getReference().child("messages");
+
+        // Initialize references to views
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        messageListView = (ListView) findViewById(R.id.messageListView);
+        photoPickerButton = (ImageButton) findViewById(R.id.photoPickerButton);
+        messageET = (EditText) findViewById(R.id.messageEditText);
+        sendButton = (Button) findViewById(R.id.sendButton);
+
+        // Initialize message ListView and its adapter
+        List<Chatty> chattyMessages = new ArrayList<>();
+        messageAdapter = new MessageAdapter(this, R.layout.item_message, chattyMessages);
+        messageListView.setAdapter(messageAdapter);
+
+        // Initialize progress bar
+        progressBar.setVisibility(ProgressBar.INVISIBLE);
+
+        // ImagePickerButton shows an image picker to upload a image for a message
+        photoPickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Fire an intent to show an image picker
+            }
+        });
+
+        // Enable Send button when there's text to send
+        messageET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().length() > 0) {
+                    sendButton.setEnabled(true);
+                } else {
+                    sendButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+        messageET.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
+
+        // Send button sends a message and clears the EditText
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Chatty chatty = new Chatty(messageET.getText().toString(), userName, null);
+                databaseReference.push().setValue(chatty);
+
+                // Clear input box
+                messageET.setText("");
+            }
+        });
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    //Toast.makeText(MainActivity.this, "You're now signed in, welcome to Chatty!", Toast.LENGTH_SHORT).show();
+                    onSignedInInitialize(user.getDisplayName());
+                }else {
+                    onSignedOutCleanUp();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // Sign-in succeeded, set up the UI
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            }else if (resultCode == RESULT_CANCELED) {
+                // Sign in was canceled by the user, finish the activity
+                Toast.makeText(this, "Sign in cancelled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (authStateListener != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
+        detachDatabaseReadListener();
+        messageAdapter.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void onSignedInInitialize(String username){
+        userName = username;
+        attachDatabaseReadListener();
+    }
+
+    private void onSignedOutCleanUp(){
+        userName = ANONYMOUS;
+        messageAdapter.clear();
+        detachDatabaseReadListener();
+    }
+
+    private void attachDatabaseReadListener(){
+        if (childEventListener == null) {
+            childEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    Chatty chatty = dataSnapshot.getValue(Chatty.class);
+                    messageAdapter.add(chatty);
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            };
+            databaseReference.addChildEventListener(childEventListener);
+        }
+    }
+
+    private void detachDatabaseReadListener(){
+        if (childEventListener != null) {
+            databaseReference.removeEventListener(childEventListener);
+            childEventListener = null;
+        }
+    }
+}
--- Juda/Club/app/src/main/java/com/example/juda/club/EditorActivity.java
+++ Juda/Club/app/src/main/java/com/example/juda/club/EditorActivity.java
@@ -1,17 +1,25 @@
 package com.example.juda.club;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -24,10 +32,26 @@ import com.example.juda.club.data.ClubContract.ClubEntry;
 import com.example.juda.club.data.ClubContract;
 import com.example.juda.club.data.ClubDBHelper;
 
+import java.nio.channels.ClosedByInterruptException;
+
 /**
      * Allows user to create a new pet or edit an existing one.
      */
-    public class EditorActivity extends AppCompatActivity {
+    public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int EXISTING_CLUB_LOADER = 0;
+
+    private boolean clubHasChanged = false;
+
+    private View.OnTouchListener touchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            clubHasChanged = true;
+            return false;
+        }
+    };
+
+    private Uri currentClubUri;
 
         private EditText clubName;
         private EditText stadiumName;
@@ -43,10 +67,11 @@ import com.example.juda.club.data.ClubDBHelper;
             setContentView(R.layout.activity_editor);
 
             Intent intent = getIntent();
-            Uri currentUri = intent.getData();
+            currentClubUri = intent.getData();
 
-            if (currentUri == null){
+            if (currentClubUri == null){
                 setTitle(getString(R.string.editor_activity_title_new_club));
+                invalidateOptionsMenu();
             }else {
                 setTitle(getString(R.string.editor_activity_title_edit_club));
             }
@@ -58,7 +83,15 @@ import com.example.juda.club.data.ClubDBHelper;
             country = (Spinner) findViewById(R.id.spinner_country);
             foundationYear = (EditText) findViewById(R.id.edit_foundation_year);
 
+            clubName.setOnTouchListener(touchListener);
+            stadiumName.setOnTouchListener(touchListener);
+            stadiumCapacity.setOnTouchListener(touchListener);
+            country.setOnTouchListener(touchListener);
+            foundationYear.setOnTouchListener(touchListener);
+
             setupSpinner();
+
+            getLoaderManager().initLoader(EXISTING_CLUB_LOADER, null, this);
         }
 
         /**
@@ -91,11 +124,17 @@ import com.example.juda.club.data.ClubDBHelper;
             });
         }
 
-        private void insertClub(){
+        private void saveClub() {
             String nameString = clubName.getText().toString().trim();
             String stadiumNameString = stadiumName.getText().toString().trim();
             int capacity = Integer.parseInt(stadiumCapacity.getText().toString().trim());
             int foundation = Integer.parseInt(foundationYear.getText().toString().trim());
+
+            if (currentClubUri == null &&
+                TextUtils.isEmpty(nameString) && TextUtils.isEmpty(stadiumNameString))
+            {
+                return;
+            }
 
             //ClubDBHelper dbHelper = new ClubDBHelper(this);
 
@@ -110,16 +149,24 @@ import com.example.juda.club.data.ClubDBHelper;
             values.put(ClubEntry.COLUMN_COUNTRY, mCountry);
             values.put(ClubEntry.COLUMN_FOUNDATION_YEAR, foundation);
 
-            // Insert a new club into the provider, returning the content URI for the new club.
-            Uri newUri = getContentResolver().insert(ClubEntry.CONTENT_URI, values);
+            // Determine if this is a new or existing pet by checking if mCurrentPetUri is null or not
+            if (currentClubUri == null) {
 
-            // Insert the new row, returning the primary key value of the new row
-            //long newRowId = db.insert(ClubEntry.TABLE_NAME, null, values);
+                // This is a NEW pet, so insert a new pet into the provider,
+                // returning the content URI for the new club.
+                Uri newUri = getContentResolver().insert(ClubEntry.CONTENT_URI, values);
 
-            if (newUri == null){
-                Toast.makeText(this, "Error with saving new club", Toast.LENGTH_SHORT).show();
-            }else {
-                Toast.makeText(this, "New club saved", Toast.LENGTH_SHORT).show();
+                if (newUri == null){
+                    Toast.makeText(this, "Error with saving new club", Toast.LENGTH_SHORT).show();
+                }else{
+                    int rowsAffected = getContentResolver().update(ClubEntry.CONTENT_URI, values, null, null);
+
+                    if (rowsAffected == 0){
+                        Toast.makeText(this, "Error with updating a club", Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(this, "The club updated", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         }
 
@@ -131,14 +178,26 @@ import com.example.juda.club.data.ClubDBHelper;
             return true;
         }
 
-        @Override
+         @Override
+         public boolean onPrepareOptionsMenu(Menu menu) {
+            super.onPrepareOptionsMenu(menu);
+
+             if (currentClubUri == null){
+                 MenuItem menuItem = menu.findItem(R.id.action_delete);
+                 menuItem.setVisible(false);
+             }
+
+             return true;
+    }
+
+    @Override
         public boolean onOptionsItemSelected(MenuItem item) {
             // User clicked on a menu option in the app bar overflow menu
             switch (item.getItemId()) {
                 // Respond to a click on the "Save" menu option
                 case R.id.action_save:
                     //Save a clubs to database
-                    insertClub();
+                    saveClub();
                     //Exit Activity
                     finish();
                     return true;
@@ -149,9 +208,115 @@ import com.example.juda.club.data.ClubDBHelper;
                 // Respond to a click on the "Up" arrow button in the app bar
                 case android.R.id.home:
                     // Navigate back to parent activity (CatalogActivity)
-                    NavUtils.navigateUpFromSameTask(this);
+                    if (!clubHasChanged) {
+                        NavUtils.navigateUpFromSameTask(EditorActivity.this);
+                        return true;
+                    }
+                    DialogInterface.OnClickListener discardButtonClickListener =
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // User clicked "Discard" button, navigate to parent activity.
+                                    NavUtils.navigateUpFromSameTask(EditorActivity.this);
+                                }
+                            };
+                    showUnsavedChangesDialog(discardButtonClickListener);
                     return true;
             }
             return super.onOptionsItemSelected(item);
         }
+
+    @Override
+    public void onBackPressed() {
+        if (!clubHasChanged) {
+            super.onBackPressed();
+            return;
+        }
+
+        DialogInterface.OnClickListener discardButtonClickListener =
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                };
+                showUnsavedChangesDialog(discardButtonClickListener);
     }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        if (currentClubUri == null){
+            return null;
+        }
+
+        String projection [] = {
+                ClubEntry._ID,
+                ClubEntry.COLUMN_CLUB_NAME,
+                ClubEntry.COLUMN_STADIUM_NAME,
+                ClubEntry.COLUMN_STADIUM_CAPACITY,
+                ClubEntry.COLUMN_COUNTRY,
+                ClubEntry.COLUMN_FOUNDATION_YEAR
+        };
+        return new CursorLoader(this,
+                currentClubUri,
+                projection,
+                null,null,null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+        if (data == null || data.getCount() < 1){
+            return;
+        }
+
+        if (data.moveToFirst()) {
+            // Find the columns of pet attributes that we're interested in
+            int clubNameColumnIndex = data.getColumnIndex(ClubEntry.COLUMN_CLUB_NAME);
+            int stadiumNameColumnIndex = data.getColumnIndex(ClubEntry.COLUMN_STADIUM_NAME);
+            int stadiumCapacityColumnIndex = data.getColumnIndex(ClubEntry.COLUMN_STADIUM_CAPACITY);
+            //int countryColumnIndex = data.getColumnIndex(ClubEntry.COLUMN_COUNTRY);
+            int foundationColumnIndex = data.getColumnIndex(ClubEntry.COLUMN_FOUNDATION_YEAR);
+
+            // Extract out the value from the Cursor for the given column index
+            String currentClubName = data.getString(clubNameColumnIndex);
+            String currentStadiumName = data.getString(stadiumNameColumnIndex);
+            int currentStadiumCapacity = data.getInt(stadiumCapacityColumnIndex);
+            //String currentCountry = data.getString(countryColumnIndex);
+            int currentFoundationYear = data.getInt(foundationColumnIndex);
+
+            // Update the views on the screen with the values from the database
+            clubName.setText(currentClubName);
+            stadiumName.setText(currentStadiumName);
+            stadiumCapacity.setText(String.valueOf(currentStadiumCapacity));
+            foundationYear.setText(String.valueOf(currentFoundationYear));
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // If the loader is invalidated, clear out all the data from the input fields.
+        clubName.setText("");
+        stadiumName.setText("");
+        stadiumCapacity.setText("");
+        foundationYear.setText("");
+    }
+
+    private void showUnsavedChangesDialog(
+        DialogInterface.OnClickListener discardButtonClickListener){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.unsaved_changes_dialog_msg);
+        builder.setPositiveButton(R.string.discard, discardButtonClickListener);
+        builder.setNegativeButton(R.string.keep_editing, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (dialog != null){
+                    dialog.dismiss();
+                }
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+}
